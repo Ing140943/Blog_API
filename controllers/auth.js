3da6aa9261ab66d3f997cd20027e4e5e28935819
Ing140943 <skladeeing@gmail.com>
2023-07-01 12:02:36 +0700

    Re-Initialize project

---
+++ controllers/auth.js
@@ -0,0 +1,55 @@
+import { db } from "../db.js"
+import bcrypt from "bcryptjs"
+
+export const register = (req, res) => {
+
+    // CHECK EXISTING USER
+    const q = "SELECT * FROM users WHERE email = ? OR username = ?";
+    db.query(q, [req.body.email, req.body.username], (err, data) => {
+          if(err) {
+            return res.json(err)
+          }
+          if(data.length) {
+            return res.status(409).json("User already exists!")
+          }
+
+          //ENCRYPT user password by bcyrptjs module
+          //Hash the password and create user (read from bcryptjs doc)
+          const salt = bcrypt.genSaltSync(10)
+          const hash = bcrypt.hashSync(req.body.password, salt)
+
+          const q = "INSERT INTO users(`username`, `email`, `password`) VALUES (?)"
+          const values = [
+            req.body.username,
+            req.body.email,
+            hash,
+          ]
+          
+          db.query(q, [values], (err, data) => {
+            if (err) return json(err)
+            return res.status(200).json("User has been created.")
+          })
+    })
+
+}
+
+export const login = (req, res) => {
+    //CHECK USER
+
+    const q  = "SELECT * FROM users WHERE username = ?"
+    db.query(q, [req.body.username], (err, data) => {
+        if (err) return json(err)
+        // If no any users
+        if (data.length == 0) return res.status(404).json("User not found!")
+         
+        //Check password (the data is array so the first element is our user)
+        console.log(data);
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password)
+
+        if(!isPasswordCorrect) return res.status(400).json("Wrong username or password")
+    })
+}
+
+export const logout = (req, res) => {
+
+}
