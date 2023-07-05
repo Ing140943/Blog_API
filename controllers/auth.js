4ebf204437d6575cf0f7ee0bb9660d701dc46e77
Ing140943 <skladeeing@gmail.com>
2023-07-05 21:38:54 +0700

    Change q name

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,8 +4,8 @@ import jwt from "jsonwebtoken";
 
 export const register = (req, res) => {
   // CHECK EXISTING USER
-  const q = `SELECT * FROM users WHERE email = $1 OR username = $2`;
-  db.query(q, [req.body.email, req.body.username], (err, data) => {
+  const select_q = `SELECT * FROM users WHERE email = $1 OR username = $2`;
+  db.query(select_q, [req.body.email, req.body.username], (err, data) => {
     if (err) {
       return res.json(err);
     }
@@ -17,9 +17,9 @@ export const register = (req, res) => {
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(req.body.password, salt);
 
-    const q = `INSERT INTO users("username", "email", "password") VALUES ($1, $2, $3)`;
+    const insert_q = `INSERT INTO users("username", "email", "password") VALUES ($1, $2, $3)`;
     const values = [req.body.username, req.body.email, hash];
-    db.query(q, [values], (err, data) => {
+    db.query(insert_q, [values], (err, data) => {
       if (err) return res.status(403).json(err);
       console.log("Reach here or not");
       return res.status(200).json("User has been created.");
@@ -30,8 +30,8 @@ export const register = (req, res) => {
 export const login = (req, res) => {
   //CHECK USER
 
-  const q = `SELECT * FROM users WHERE username = $1`;
-  db.query(q, [req.body.username], (err, data) => {
+  const select_q = `SELECT * FROM users WHERE username = $1`;
+  db.query(select_q, [req.body.username], (err, data) => {
     if (err) return json(err);
     // If no any users
     if (data.length == 0) return res.status(404).json("User not found!");
