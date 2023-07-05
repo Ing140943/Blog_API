852361778a6821a295bfc87afe1f4b7258289e8f
Ing140943 <skladeeing@gmail.com>
2023-07-05 15:48:46 +0700

    Handle cors error

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import postRoutes from "./routes/posts.js"
 import cookieParser from "cookie-parser"
 import multer from "multer";
 import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express()
 dotenv.config()
@@ -12,6 +13,19 @@ dotenv.config()
 app.use(express.json())
 app.use(cookieParser())
 
+
+const whitelist = ["https://anime-setthant-blog-d51535ba59d0.herokuapp.com"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
 // For uploading in the machine
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
