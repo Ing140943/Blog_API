0765141f59f79892133fbb12e5c9a21f98c2701d
Ing140943 <skladeeing@gmail.com>
2023-07-11 23:51:16 +0700

    Update cookie parser

--- controllers/auth.js
+++ controllers/auth.js
@@ -52,7 +52,10 @@ export const login = async (req, res) => {
 
     res
       .cookie("access_token", token, {
-        httpOnly: true,
+        sameSite : "none",
+        secure: true,
+        domain: "https://anime-setthant-blog-d51535ba59d0.herokuapp.com/",
+        httpOnly: true
       })
       .status(200)
       .json(other);
